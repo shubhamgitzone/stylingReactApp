685b730204566af010030493ed39d22f55db1647
shubham kumar <shubhamkmuz@gmail.com>
2025-10-30 18:11:03 +0530

    Introducing & symbol in styled component to collectively define styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -45,6 +45,20 @@ const Input = styled.input`
 // but it requires additional configuration in babel or typescript
 // So we are using 'invalid' prop directly for simplicity
 
+const ButtonContainer = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -99,9 +113,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <ButtonContainer onClick={handleLogin}>Sign In</ButtonContainer>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,71 @@
 import logo from "../assets/logo.png";
-import classes from "./Header.module.css";
+// import classes from "./Header.module.css";
+import { styled } from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Pacifico", cursive;
+    margin: 0;
+  }
+
+  .& p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    & {
+      margin-bottom: 4rem;
+    }
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+`;
+
+// & is used to target the current component in styled-components
+// like HeaderContainer in this case.
+// It helps to avoid naming conflicts and keeps styles scoped to the component.
+// For more details, refer to the 'styled-components' documentation.
+//  It stye header element as well as its child elements like img, h1 and p.
 
 export default function Header() {
   return (
-    <header>
+    <HeaderContainer>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       {/* passing empty style object to demo inline styling */}
       <p
-        className={classes.paragraphs}
-        style={{
-          color: "red",
-          textAlign: "left",
-        }}
+      // className={classes.paragraphs}
+      // style={{
+      //   color: "red",
+      //   textAlign: "left",
+      // }}
       >
         A community of artists and art-lovers.
       </p>
-    </header>
+    </HeaderContainer>
   );
 }
